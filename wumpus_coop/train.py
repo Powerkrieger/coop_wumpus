27ca4dd5e33573e13b0b714a0ca484aac35caacf
Power <joost.rehder@uni-rostock.de>
2021-10-22 13:56:46 +0200

    insertions of time module

--- wumpus_coop/train.py
+++ wumpus_coop/train.py
@@ -1,22 +1,36 @@
 import gym
 import wumpus_env
 import random
+import time
+from datetime import timedelta
 
-env = gym.make('wumpus-v0')
-#states = env.observation_space.shape[0]
+def main():
+    start_time = time.time()
+    env = gym.make('wumpus-v0')
 
+    #random environment
+    episodes = 10
+    for episode in range(1, episodes+1):
+        state = env.reset()
+        done = False
+        score = 0
 
-#random environment
-episodes = 10
-for episode in range(1, episodes+1):
-    state = env.reset()
-    done = False
-    score = 0
+        while not done:
+            env.render()
+            action = random.choice(env.action_space)
+            print(action)
+            n_state, reward, done, info = env.step(action)
+            score += reward
+        print('Episode:{} Score:{}'.format(episode, score))
 
-    while not done:
-        env.render()
-        action = random.choice(env.action_space)
-        print(action)
-        n_state, reward, done, info = env.step(action)
-        score += reward
-    print('Episode:{} Score:{}'.format(episode, score))
+    elapsed_time_secs = time.time() - start_time
+    msg = "Execution took: %s secs (Wall clock time)" % timedelta(seconds=round(elapsed_time_secs))
+    with open('times.txt', 'a') as f:
+        f.write(datetime.fromtimestamp(start_time))
+        #for i in printargs:
+        #    f.write(i + '\n')
+        f.write(msg + '\n')
+        f.write('end' + '\n')
+
+if __name__ == '__main__':
+main()
