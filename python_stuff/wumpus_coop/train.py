6dde4b86edd090f429def7fcc1eb28585bc71791
ns476 <norbert.scheffler@uni-rostock.de>
2021-12-13 19:42:07 +0100

    restore train.py

--- python_stuff/wumpus_coop/train.py
+++ python_stuff/wumpus_coop/train.py
@@ -4,7 +4,6 @@ import random
 import time
 import datetime
 from datetime import timedelta
-import numpy as np
 
 def main():
     # for saving timestamp and elapsed time
@@ -13,84 +12,20 @@ def main():
 
     env = gym.make('wumpus-v0')
 
-    # Init QTable
-    qTable = [[]] * 32
-
-    for x in range(len(qTable)):
-        qTable[x] = np.array(np.arange(9))
-
-    for x in range(len(qTable)):
-        for y in range(len(qTable[x])):
-            qTable[x][y] = 0
-
-    allscore = 0
-
-    # random environment
-    episodes = 1000
+    #random environment
+    episodes = 10
     for episode in range(1, episodes+1):
         state = env.reset()
         done = False
         score = 0
 
-        actions = env.action_space
-        action = 'Nothing'
-        n_state, reward, done, info = env.step(action)
-
-        agentactions = []
-        agentconfigs = []
-        fullreward = 0
-
         while not done:
             env.render()
-
-            confignum = 0
-            for x in range(len(n_state)):
-                if n_state[x]:
-                    confignum = confignum + np.power(2, x)
-
-            rand = random.randint(0, 100)
-            if rand < 30 or not episode > 900:
-                action = random.choice(env.action_space)
-            else:
-                actnr = 0
-                actmaxrew = 0
-                for x in range(len(qTable[confignum])):
-                    if actmaxrew <= qTable[confignum][x]:
-                        actnr = x
-                        actmaxrew = qTable[confignum][x]
-
-                action = actions[actnr]
-
-            agentconfigs.append([n_state])
-            agentactions.append(action)
-
+            action = random.choice(env.action_space)
             print(action)
-
             n_state, reward, done, info = env.step(action)
             score += reward
-
-            fullreward += reward
-
-            if rand < 30 or not episode > 900:
-                qTable[confignum][actions.index(action)] = (qTable[confignum][actions.index(action)] + reward) / 2
-            else:
-                qTable[confignum][actnr] = (qTable[confignum][actnr] + reward) / 2
-
         print('Episode:{} Score:{}'.format(episode, score))
-        allscore += score
-
-        for a in range(len(agentconfigs)):
-
-            confignum = 0
-            for x in range(len(agentconfigs[a])):
-                if agentconfigs[a][x]:
-                    confignum = confignum + np.power(2, x)
-
-            laction = agentactions[a]
-            qTable[confignum][actions.index(laction)] = (qTable[confignum][actions.index(laction)] + fullreward/10 ) / 2
-
-    print(qTable)
-    print(allscore)
 
     elapsed_time_secs = time.time() - start_time
     msg = "Execution took: %s secs (Wall clock time)" % timedelta(seconds=round(elapsed_time_secs))
